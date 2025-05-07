2d0202d4e21a46dacddf031ee51017783b2d0fc3
Larshle <larshle@stud.ntnu.no>
2025-05-06 19:58:45 -0700

    Ssh onto lab pc to drive turtlebot

--- bcast/cast.py
+++ bcast/cast.py
@@ -2,29 +2,32 @@ import subprocess
 from pathlib import Path
 import os
 
-# → path to your existing script
-local_path = Path("/Users/larsleopold/Documents/ASL_recognition_yolo/Real_time_asl_detector/asl_cam/generated_commands/CIRCLE.py")
+local_path = Path("/Users/larsleopold/Documents/ASL_recognition_yolo/Real_time_asl_detector/asl_cam/generated_commands/command.py")
 
-USER           = "ee106b-abk"
+USER           = "ee106b-acg"
 HOST           = "128.32.40.228"
-REMOTE_DIR     = "/home/cc/ee106b/sp25/class/ee106b-abk/Documents/206B-projects/receive"
+REMOTE_DIR     = "/home/cc/ee106b/sp25/class/ee106b-acg/Desktop/typeshit/src/asl-pkg/scripts"
 KEY_PATH       = "~/.ssh/robot6969"
-RUN_AFTER_COPY = False
+RUN_AFTER_COPY = True
+
+remote_cmd = (
+    "cd /home/cc/ee106b/sp25/class/ee106b-acg/Desktop/typeshit && "
+    "source devel/setup.bash && "
+    "rosrun asl-pkg command.py"
+)
 
 remote_path = f"{USER}@{HOST}:{REMOTE_DIR}/{local_path.name}"
-
-# 1) Copy it:
 subprocess.run([
     "scp", "-i", os.path.expanduser(KEY_PATH),
     str(local_path), remote_path
 ], check=True)
 
-# 2) (Optionally) run it remotely:
 if RUN_AFTER_COPY:
-    subprocess.run([
-        "ssh", "-i", os.path.expanduser(KEY_PATH),
-        f"{USER}@{HOST}",
-        f"python3 {REMOTE_DIR}/{local_path.name}"
-    ], check=True)
+    subprocess.run(
+        f"ssh -i {os.path.expanduser(KEY_PATH)} {USER}@{HOST} "
+        f"\"bash -lc '{remote_cmd}'\"",
+        shell=True,
+        check=True
+    )
 
 print("Done.")
